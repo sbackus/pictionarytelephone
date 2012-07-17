65d1f30e8863820c5479e398437c2068db24ba1e
Sam Backus <Samuel.Backus@gmail.com>
2012-07-17 18:42:09 -0400

    adding archive view and post view

--- pictionarytelephone/blog/urls.py
+++ pictionarytelephone/blog/urls.py
@@ -1,9 +1,17 @@
 from django.conf.urls import patterns, include, url
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from blog.models import Post
 
-urlpatterns = patterns('',
-    url(r'^',ListView.as_view(
+urlpatterns = patterns('blog.views',
+    url(r'^$',ListView.as_view(
                 queryset=Post.objects.all().order_by("-created")[:2],
 		template_name="blog.html")),
+    url(r'^(?P<pk>\d+)',DetailView.as_view(
+                model = Post,
+		template_name="post.html")),
+    url(r'^archives',ListView.as_view(
+                queryset=Post.objects.all().order_by("-created"),
+		template_name="archives.html")),	
+		
+		
 )
