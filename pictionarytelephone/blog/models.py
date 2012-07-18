bc844c7b09f56ad9c86178cfa00821cdd7e95948
sbackus <Samuel.Backus@gmail.com>
2012-07-18 08:58:53 -0400

    adding images to the model and refreshing the DB

--- pictionarytelephone/blog/models.py
+++ pictionarytelephone/blog/models.py
@@ -3,10 +3,11 @@ from taggit.managers import TaggableManager
 
 class Post(models.Model):
   title = models.CharField(max_length=100)
-#  image = 
+  image = models.ImageField("uploaded image", upload_to="images/%Y/%m/%d", blank=True, null=True)
   caption = models.TextField()
   created = models.DateTimeField()
   tags = TaggableManager()
+  
   
   def __unicode__(self):
     return self.title
