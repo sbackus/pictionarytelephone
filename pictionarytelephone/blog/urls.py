755353924efd0303fad6c03eb8baafd7bb971269
sbackus <Samuel.Backus@gmail.com>
2012-07-18 00:13:37 -0400

    fixing tagging and adding rss feed
    Here ends the Building a blog in 30 mins with Django Screencast
    http://www.youtube.com/watch?v=srHZoj3ASmk

--- pictionarytelephone/blog/urls.py
+++ pictionarytelephone/blog/urls.py
@@ -1,7 +1,23 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import ListView, DetailView
 from blog.models import Post
+from django.contrib.syndication.views import Feed
 
+class BlogFeed(Feed):
+    title = "PictionaryTelephone"
+    description = "A project by Sam Backus to document the awesome creativity and humor of the people who play the game "
+    link = "/blog/feed/"
+	
+    def items(self):
+        return Post.objects.all().order_by("-created")[:2]
+    def item_title(self, item):
+        return item.title
+    def item_description(self, item):
+        return item.caption
+    def item_link(self, item):
+         return u"/blog/%d" % item.id
+	
+	
 urlpatterns = patterns('blog.views',
     url(r'^$',ListView.as_view(
                 queryset=Post.objects.all().order_by("-created")[:2],
@@ -12,6 +28,6 @@ urlpatterns = patterns('blog.views',
     url(r'^archives',ListView.as_view(
                 queryset=Post.objects.all().order_by("-created"),
 		template_name="archives.html")),	
-		
-		
+    url(r'^tag/(?P<tag>\w+)$', 'tagpage'),
+	url(r'^feed', BlogFeed()),
 )
--- pictionarytelephone/blog/views.py
+++ pictionarytelephone/blog/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+from blog.models import Post
+from django.shortcuts import render_to_response
+
+
+def tagpage(request,tag):
+    posts = Post.objects.filter(tags__name=tag)
+    return render_to_response("tagpage.html", {"posts":posts, "tag":tag})
